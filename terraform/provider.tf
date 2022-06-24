ba3da5dae83389db0332a20063ce796f8ccdfe64
Srikanth Pathipati <srikanth.pathipati@cambio.se>
2022-06-24 12:47:03 +0200

    gke-infra update

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -3,12 +3,6 @@ provider "google" {
   project = "nord-project"
   region  = "us-central1"
 }
-
-terraform {
-  backend "gcs" {
-    bucket = "nord-project-gke-tf"
-    prefix = "terraform/state"
-  }
   
   required_providers {
     google = {
